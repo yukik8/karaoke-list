585216e16fa9e6d8a2b81a66f81e2458498ac31e
yukik8 <andres8.yuuki@gmail.com>
2023-09-30 20:59:20 +0900

    マイソングページの履歴欄の修正、テスト広告の無効化

--- lib/assets/constants/privacy_policy.dart
+++ lib/assets/constants/privacy_policy.dart
@@ -1,15 +1,15 @@
 // ignore: camel_case_types
 class PrivacyPolicy {
   String privacyText = '''
-プライバシーポリシー
+以下は、Yuuki.Kが開発した本アプリKaraLisで提供されるサービスに関するプライバシーポリシーです。
 
 
 
 情報の収集と使用
 
-個人を特定できる情報を第三者に提供することはございません。
+アプリ内広告用いられる匿名ID並びにご利用端末情報を取得しております。
 
-ユーザーのご利用端末を解析し、アプリの改善のために利用します。
+上記以外の個人情報は利用しておらず、個人を特定できる情報を第三者に提供することはございません。
 
 
 
@@ -22,5 +22,11 @@ class PrivacyPolicy {
 お問い合わせ
 
 お問い合わせは、andres8.yuuki@gmail.comまでお願いいたします。
+
+
+
+プライバシーポリシーのリンク
+
+https://glen-plastic-144.notion.site/KaraLis-f4a2506f05c1489498aba670ff479764?pvs=4](https://www.notion.so/KaraLis-f4a2506f05c1489498aba670ff479764?pvs=21
   ''';
 }
--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
 import '../assets/constants/privacy_policy.dart';
 import '../assets/constants/terms_of_service.dart';
@@ -16,26 +16,13 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   bool hasError = false;
   String version = '';
-  late SharedPreferences? prefs;
-  bool accessTokenIsSaved = false;
 
   @override
   void initState() {
     Future(() async {
-      await getAccessToken();
       await getVersion();
     });
     super.initState();
-  }
-
-  Future<void> getAccessToken() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final fetchedAccessToken = prefs.getString('token') ?? "";
-    final isSaved = fetchedAccessToken != "";
-
-    setState(() {
-      accessTokenIsSaved = isSaved;
-    });
   }
 
   Future<void> getVersion() async {
@@ -50,6 +37,7 @@ class _SettingPageState extends State<SettingPage> {
     return Scaffold(
       backgroundColor: const Color(0xffFAFAFA),
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
         title: const Text("Settings",
         style: TextStyle(color: Color(0xffC57E14)),)),
@@ -110,23 +98,22 @@ class _SettingPageState extends State<SettingPage> {
           const SizedBox(
             height: 36,
           ),
-          if (accessTokenIsSaved)
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '　その他',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xff828282),
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-              ],
-            ),
+            // const Column(
+            //   crossAxisAlignment: CrossAxisAlignment.start,
+            //   children: [
+            //     Text(
+            //       '　その他',
+            //       style: TextStyle(
+            //         fontSize: 12,
+            //         fontWeight: FontWeight.w500,
+            //         color: Color(0xff828282),
+            //       ),
+            //     ),
+            //     SizedBox(
+            //       height: 8,
+            //     ),
+            //   ],
+            // ),
         ],
       ),
     );
